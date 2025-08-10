f2e69d18026eefcc2f2d19721883de2bed245b26
amdadul haque <amdadulhaque601671@gmail.com>
2025-08-10 11:05:10 +0600

    boocard component has many changes

--- src/pages/BookShelf/BookCard.jsx
+++ src/pages/BookShelf/BookCard.jsx
@@ -20,6 +20,7 @@ const BookCard = ({ book }) => {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
+            
             className="rounded-2xl shadow-md dark:border dark:border-gray-600 hover:shadow-xl transition-shadow duration-300 overflow-hidden">
             <figure className="w-full h-64 overflow-hidden">
                 <img
